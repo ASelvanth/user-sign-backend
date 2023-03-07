2cbc912093f34838ce103820255113aa3e2c5586
ASelvanth <ajselva04@gmail.com>
2023-03-07 11:24:48 +0530

    cors added

--- index.js
+++ index.js
@@ -16,10 +16,17 @@ db();
 app.use(express.json());
 app.use(cookieParser());
 // app.use(cors());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true
+// }));
+
+const corsOrigin ={
+    origin:'http://localhost:3000', //or whatever port your frontend is using
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
 
 app.get('/',(req,res)=>{
     res.send("Home Page");
