2ec8ffa8fd6d37ba33f601ad025b21185821403b
ASelvanth <ajselva04@gmail.com>
2023-03-07 11:13:59 +0530

    cors updated

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ db();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true
-// }));
+// app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 
 app.get('/',(req,res)=>{
     res.send("Home Page");
